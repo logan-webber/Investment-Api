bfa5b17d2164ea650ec458c38a27f9fb81b10068
Logan Webber <logan.webber@hotmail.com>
2021-05-09 18:08:45 +1200

    new api call attempt

--- client/api.js
+++ client/api.js
@@ -1,9 +1,25 @@
 import request from 'superagent'
 
-const baseUrl = 'https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/'
+// const baseUrl = 'https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/'
 const symbol = 'AMC'
 
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "20d0546455msh063453ff8acb61fp17fb6ajsn286d50e62a0e",
+		"x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com"
+	}
+};
+
+
+ajax(settings).done(function (response) {
+	console.log(response);
+});
+
 export function getSummary () {
-  return request.get(baseUrl + 'get-summary?symbol=' + symbol + '&regionUS')
+  return request.get(settings[2] + 'get-summary?symbol=' + symbol + '&regionUS')
     .then(res => res.body)
 }
