049c4906d3a3e01ae23320017492d60b2c58e3f1
logan <logan.webber@hotmail.com>
2021-05-10 21:47:50 +1200

    another attempt but unsuccessful

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { getSummary } from '../api'
 
 const App = () => {
+
+  useEffect((
+    getSummary()
+  ), [])
+
   return (
     <>
       <h1>React development has begun!</h1>
-      <p>{getSummary()}</p>
+      {/* <p>{getSummary()}</p> */}
     </>
   )
 }
